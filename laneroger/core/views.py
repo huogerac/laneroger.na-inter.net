e1e00355ec55ea345165e78740be81135ce8b576
Roger Camargo <roger.camargo@djenie.com>
2015-01-17 13:46:43 -0200

    First page content

--- laneroger/core/views.py
+++ laneroger/core/views.py
@@ -7,5 +7,7 @@ class ShowHomeView(TemplateView):
     template_name = "core/home.html"
 
     def get_context_data(self, **kwargs):
+    	to = self.request.GET.get("to")
         context = super(ShowHomeView, self).get_context_data(**kwargs)
+        context['to'] = to
         return context
